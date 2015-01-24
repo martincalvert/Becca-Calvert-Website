727c6bc2f4e002a44d6cbd5ec0b5a8e63e2b50da
Martin <martinlcalvert@gmail.com>
2015-01-23 19:09:17 -0800

    Pre: Exp layout

---
+++ expierence.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+    <?php include 'head.php' ?>
+    <title>Becca Calvert - Pastry Artist - Expierence</title>
+  </head>
+  <body>
+    <div class='container'>
+      <?php include 'nav.php' ?>
+      <div class='col-xs-12 body text-center'>
+        <h2>Expierence</h2>
+        <p>Copy Needed</p>
+        <h2>Schooling</h2>
+              <p>
+                <ul style="list-style:none;">
+                  <li><i class="fa fa-birthday-cake"></i> Woodcrest Christian High School (2005-2008)</li>
+                  <li><i class="fa fa-birthday-cake"></i> Art Institute - Culinary Arts (2010-2011)</li>
+                </ul>
+              </p>
+              <h2>Expierence</h2>
+              <p>
+                <ul style="list-style:none;">
+                  <li><i class="fa fa-birthday-cake"></i> Cake Decorator - Ralphs (2009-2010)</li>
+                  <li><i class="fa fa-birthday-cake"></i> Head Cake Decorator - Creme De La Creme (2010-2014)
+                  <li><i class="fa fa-birthday-cake"></i> Tons of freelance orders</li>
+                </ul>
+              </p>
+      </div>
+    </div>
+    <?php include 'footer.php' ?>
+    <script>
+      $(function(){
+        $('[href = "expierence.php"]').addClass('active-link');
+      });
+    </script>
+  </body>
+</html>
