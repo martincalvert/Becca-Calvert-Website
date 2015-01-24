403d3d762b5a2bd653424d7a24029f233bbfebe9
Martin <martinlcalvert@gmail.com>
2015-01-23 19:08:16 -0800

    Pre: About layout

--- about.php
+++ about.php
@@ -1,36 +1,21 @@
-<?php include 'head.php'?>
-          <div class="row color-box-lt text-right">
-            <div class="col-xs-12 lead">
-              <h1>About Myself</h1>
-              <p>Food it is just a great thing, it is my passion, but even more so dessert.</p>
-            </div>
-          </div>
-          <div class="row color-box-md text-right">
-            <div class="col-xs-12 lead">
-              <h2>Schooling</h2>
-              <p>
-                <ul style="list-style:none;">
-                  <li><i class="fa fa-birthday-cake"></i> Woodcrest Christian High School (2005-2008)</li>
-                  <li><i class="fa fa-birthday-cake"></i> Art Institute - Culinary Arts (2010-2011)</li>
-                </ul>
-              </p>
-            </div>
-          </div>
-          <div class="row color-box-dk text-right">
-            <div class="col-xs-12 lead">
-              <h2>Expierence</h2>
-              <p>
-                <ul style="list-style:none;">
-                  <li><i class="fa fa-birthday-cake"></i> Cake Decorator - Ralphs (2009-2010)</li>
-                  <li><i class="fa fa-birthday-cake"></i> Head Cake Decorator - Creme De La Creme (2010-2014)
-                  <li><i class="fa fa-birthday-cake"></i> Tons of freelance orders</li>
-                </ul>
-              </p>
-            </div>
-          </div>
-        <script>
-        $(function(){
-          $('.fa-user').parent().addClass('active-link');
-        });
-        </script>
-<?php include 'footer.php'?>
+<html>
+  <head>
+    <?php include 'head.php' ?>
+    <title>Becca Calvert - Pastry Artist - About Me</title>
+  </head>
+  <body>
+    <div class='container'>
+      <?php include 'nav.php' ?>
+      <div class='col-xs-12 body text-center'>
+        <h2>About Myself</h2>
+        <p>Copy Needed</p>
+      </div>
+    </div>
+    <?php include 'footer.php' ?>
+    <script>
+      $(function(){
+        $('[href = "about.php"]').addClass('active-link');
+      });
+    </script>
+  </body>
+</html>
