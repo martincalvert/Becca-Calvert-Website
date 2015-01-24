1270b7682781f4666c9dfd46a5ee76cd05fbbe52
Martin <martinlcalvert@gmail.com>
2015-01-23 18:25:46 -0800

    Pre: Nav complete

---
+++ nav.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav">
+        <span class="sr-only">Toggle navigation</span>
+        <i class="fa fa-2x fa-bars"></i>
+      </button>
+      <a href="#" class="hidden-lg hidden-md hidden-sm name navbar-brand">Becca Calvert</a>
+    </div>
+    <div class="collapse navbar-collapse" id="nav">
+      <ul class="nav navbar-nav">
+        <li class="hidden-lg hidden-md hidden-sm"><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="experience.php">Expierence</a></li>
+        <li class="hidden-xs"><a class="name" href="#">Becca Calvert</a></li>
+        <li><a href="portfolio.php">Portfolio</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container -->
+</nav>
